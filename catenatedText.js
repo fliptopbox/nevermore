926abf46d80cfc4a0916074cf8459e655b8a7546
Bruce Thomas <response.write@gmail.com>
2022-03-29 12:59:31 +0100

    fix typo

--- catenatedText.js
+++ catenatedText.js
@@ -1,5 +1,5 @@
 
-const data = await fetch("/data.json").then(d = d.json());
+const data = await fetch("/data.json").then(d => d.json());
 const everything = Object.keys(data).map(k => data[k]).flat()
 .sort()
 .filter((item, i, array) => array.indexOf(item) === i)
